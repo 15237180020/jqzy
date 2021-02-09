3e3832c577990cb731547480d8c63609f0a9e42e
龙泽源 <8041518+long-zeyuan@user.noreply.gitee.com>
2021-02-09 16:36:10 +0800

    查询分页

--- src/views/dh/userList.vue
+++ src/views/dh/userList.vue
@@ -1,23 +1,43 @@
 <template>
   <div>
-    <el-table :data="nav" style="width: 100%">
-      <el-table-column prop="id" label="ID"> </el-table-column>
-      <el-table-column prop="username" label="用户名"> </el-table-column>
-      <el-table-column prop="email" label="邮箱"> </el-table-column>
+    <el-table
+      :data="nav"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="id"
+        label="ID"
+      > </el-table-column>
+      <el-table-column
+        prop="username"
+        label="用户名"
+      > </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+      > </el-table-column>
 
-      <el-table-column prop="phone" label="电话"> </el-table-column>
+      <el-table-column
+        prop="phone"
+        label="电话"
+      > </el-table-column>
 
-      <el-table-column prop="createTime" :formatter="onformatter" label="注册时间"></el-table-column>
+      <el-table-column
+        prop="createTime"
+        :formatter="onformatter"
+        label="注册时间"
+      ></el-table-column>
     </el-table>
- <div class="block">
-    <span class="demonstration">直接前往</span>
-    <el-pagination
-      @current-change="handleCurrentChange"
-      :page-size="100"
-      layout="prev, pager, next, jumper"
-      :total="1000">
-    </el-pagination>
-  </div>
+    <div class="block">
+      <span class="demonstration">直接前往</span>
+      <el-pagination
+        @current-change="handleCurrentChange"
+        :page-size="100"
+        layout="prev, pager, next, jumper"
+        :total="1000"
+      >
+      </el-pagination>
+    </div>
   </div>
 </template>
 
@@ -26,7 +46,7 @@
 import { userList } from "../../http/index";
 import time from '../../uiif/Time'
 export default {
-  data() {
+  data () {
     return {
       list: {
         pageNum: 5,
@@ -37,39 +57,41 @@ export default {
   },
 
   methods: {
-    async liatdata() {
-      const {data:res}  = await userList(
+    async liatdata () {
+      const { data: res } = await userList(
         `/api/user/list.do?pageNum=${this.list.pageNum}`
       );
 
       console.log(res)
 
-      this.nav = res.data.list;      
-       
+      this.nav = res.data.list;
+
       if (res.status !== 0) return this.$message.error(res.msg);
 
-      
+
     },
 
-    handleCurrentChange(val){
-        // console.log(val)
-        this.list.pageNum = val;
-        this.liatdata()
+    handleCurrentChange (val) {
+      console.log(val)
+      this.list.pageNum = val;
+      this.liatdata()
     },
 
 
-    onformatter(row, column, cellValue, index){
-      // console.log(row.updateTime)
+    onformatter (row, column, cellValue, index) {
+
       return time(row.updateTime)
     }
   },
-  mounted() {
+  mounted () {
     this.liatdata();
   },
+  created () {
 
+  },
 
-  components:{
-      
+  components: {
+
   }
 };
 </script>
